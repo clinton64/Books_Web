4b423c53a70afac13d8b76dda69a69d34ba35d1b
Clinton <clintonchakma.24csedu.064@gmail.com>
2023-05-25 12:50:04 +0600

    Extended IdentityUser

---
+++ BulkyBook.Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { set; get; }
+        public string? StreetAddress { set; get; }
+        public string? City { set; get;}
+        public string? State { set; get; }
+        public string? PostalCode { set; get;}
+    }
+}
