2eb4eb2a0a94080c8ccd62772d3bee18d2b8418c
Clinton <clintonchakma.24csedu.064@gmail.com>
2023-05-25 13:19:32 +0600

    1: Created Company model 2: Add Company to Repository and UnitOfWork

---
+++ BulkyBook.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,24 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _context = db;
+        }
+
+        public void Update(Company company)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -10,10 +10,12 @@ namespace BulkyBook.DataAccess.Repository
             Category = new CategoryRepository(_context);
             CoverType = new CoverRepository(_context);
             Product = new ProductRepository(_context);
+            Company = new CompanyRepository(_context);
         }
         public ICategoryRepository Category { get; private set; }
         public ICoverTypeRepository CoverType { get; private set; }
         public IProductRepository Product { get; private set; }
+        public ICompanyRepository Company { get; private set; }
         public void Save()
         {
             _context.SaveChanges();
